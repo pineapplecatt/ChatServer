c6490982040dac4f56e28f4fc585515bcd755977
PineappleCatt <2363801645@qq.com>
2023-08-20 15:29:14 +0800

    update

---
+++ Server.h
@@ -0,0 +1,28 @@
+#ifndef CHATROOM_SERVER_H
+#define CHATROOM_SERVER_H
+
+#include <netinet/in.h>
+#include <stdlib.h>
+#include <list>
+#include <string>
+
+#include "Common.h"
+
+using namespace std;
+
+class Server {
+   public:
+    Server();
+    void init();
+    void close();
+    void start();
+
+   private:
+    int sendBroadcastMessage(int clientfd);
+    sockaddr_in serverAddr;
+    int listener;
+    int epfd;
+    list<int> clients_list;
+};
+
+#endif  // CHATROOM_SERVER_H
